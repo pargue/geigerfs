e19ff12b99df12f7354c24215c6c244f843a3696
arguepr <paul_r_argue@whirlpool.com>
2017-04-08 21:54:03 -0400

    finished cpm file

--- GeigerFSTests/GeigerFSTests.py
+++ GeigerFSTests/GeigerFSTests.py
@@ -4,16 +4,22 @@ Created on Mar 13, 2017
 '''
 import unittest
 import sys
+from mock import mock_open
 sys.path.append('..')
+import logging
 from geigerfs import GeigerFS
-
+import time
+import os
 
 class GeigerFSTest(unittest.TestCase):
 
     def test_getattr_returns_attrs_dict_for_root(self):
+        with open('test.txt', 'w') as tfh:
+            tfh.write('')
         for key in ('st_atime', 'st_ctime',  
                  'st_mode', 'st_mtime', 'st_nlink'):
             self.assertIn(key, self.root_gfs.getattr('/', None).keys())
+        os.remove('test.txt')
 
 
     def test_readdir_includes_random_cpm_curr_parent(self):
@@ -21,35 +27,89 @@ class GeigerFSTest(unittest.TestCase):
             self.assertIn(key, self.root_gfs.readdir('/', None) )
 
 
-    @unittest.skip("TODO")
-    def test_cpm_returns_0p5_after_120s_interval(self):
-        # TODO create data for 120s interval
+    def test_cpm_handles_empty_times_file(self):
+        with open('test.txt', 'w') as tfh:
+            tfh.write('')
+
         fh = self.root_gfs.open("/cpm", 0)
-        self.assertEqual(self.root_gfs.read("/cpm", 256, 0, fh), "0.5")
+        cpm = self.root_gfs.read("/cpm", 256, 0, fh)
+        self.assertEqual(cpm, '0\n')
+        os.remove('test.txt')
 
 
-    @unittest.skip("TODO")
-    def test_cpm_returns_1_after_60s_interval(self):
-        # TODO create data for 60s interval
+    def test_cpm_returns_1_with_120s_interval(self):
+#         mocked_open_function = mock.mock_open(read_data=my_text)
+#         with mock.patch("__builtin__.open", mocked_open_function):
+#             with open("any_string") as f:
+#                 print f.read()
+#         open_name = '%s.open' % __name__
+#         with patch(open_name, create=True) as mock_open:
+#             mock_open.return_value = MagicMock(spec=file)
+#
+#             with open('test.txt', 'w') as f:
+#                 f.readline()
+#
+        self.make_test_file(120)
         fh = self.root_gfs.open("/cpm", 0)
-        self.assertEqual(self.root_gfs.read("/cpm", 256, 0, fh), "1")
+        cpm = self.root_gfs.read("/cpm", 256, 0, fh)
+        self.assertEqual(cpm, '1\n')
+        os.remove('test.txt')
 
 
-    @unittest.skip("TODO")
-    def test_cpm_returns_2_after_30s_interval(self):
-        # TODO create data for 30s interval
+    def test_cpm_returns_1_with_60s_interval(self):
+        self.make_test_file(60)
         fh = self.root_gfs.open("/cpm", 0)
-        self.assertEqual(self.root_gfs.read("/cpm", 256, 0, fh), "2")
+        cpm = self.root_gfs.read("/cpm", 256, 0, fh)
+        self.assertEqual(cpm, '1\n')
+        os.remove('test.txt')
+
+
+    def test_cpm_returns_2_with_30s_intervals(self):
+        self.make_test_file(30)
+        fh = self.root_gfs.open("/cpm", 0)
+        cpm = self.root_gfs.read("/cpm", 256, 0, fh)
+        self.assertEqual(cpm, '2\n')
+        os.remove('test.txt')
+
+
+    def test_cpm_returns_60_with_big_file(self):
+        self.make_big_test_file(1024)
+        fh = self.root_gfs.open("/cpm", 0)
+        cpm = self.root_gfs.read("/cpm", 256, 0, fh)
+        self.assertEqual(cpm, '60\n')
+        os.remove('test.txt')
 
 
     def setUp(self):
-        self.root_gfs = GeigerFS()
+        self.root_gfs = GeigerFS('test.txt')
 
 
     def tearDown(self):
         pass
 
 
+    def make_test_file(self, interval):
+        t = time.time()
+        my_text = str(t) + '\n'
+        for i in range(60/interval):
+            t += interval
+            my_text += str(t) + '\n'
+        f = open('test.txt', 'w')
+        f.write(my_text)
+        f.close()
+
+
+    def make_big_test_file(self, int_count):
+        t = time.time()
+        my_text = str(t) + '\n'
+        for i in range(int_count):
+            my_text += str(t + i) + '\n'
+        f = open('test.txt', 'w')
+        f.write(my_text)
+        f.close()
+
+
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
--- geigerfs.py
+++ geigerfs.py
@@ -4,36 +4,45 @@ from __future__ import with_statement, absolute_import
 
 import time
 import logging
+import os
 
-from stat import S_IFDIR, S_IFREG
+from stat import S_IFDIR, S_IFREG, S_IFCHR
 from errno import ENOENT
 from sys import argv, exit
 from fuse import FUSE, FuseOSError, Operations, LoggingMixIn
 
 class GeigerFS(LoggingMixIn, Operations):
-    def __init__(self):
-        self.files = {}
-        self.data = {}
-        self.reads = {}
+    def __init__(self, harvestFile='times.txt'):
+        self.files = {} # file stats for each vfile
+        self.data = {}  # data str for each vfile
+        self.reads = {} # handlers for the read operation for each vfile
+        self.timesfhs = {}  # file handles for the times.txt file for each vfile
+
         now = time.time()
         self.data['/random'] = ""
-        self.data['/cpm'] = "2"
+        self.data['/cpm'] = "0"
         self.files['/'] = dict(st_mode=(S_IFDIR | 0o755), st_ctime=now,
                                st_mtime=now, st_atime=now, st_nlink=2)
         self.files['/random'] = dict(st_mode=(S_IFREG | 0o444), st_ctime=now,
-                               st_mtime=now, st_atime=now, st_nlink=2)
+                               st_mtime=now, st_atime=now, st_nlink=1)
         self.files['/cpm'] = dict(st_mode=(S_IFREG | 0o444), st_ctime=now,
-                               st_mtime=now, st_atime=now, st_nlink=2,
-                               st_size=len(self.data['/cpm']))
+                               st_mtime=now, st_atime=now, st_nlink=1,
+                               st_size=3)
+
         self.reads['/cpm'] = self.doReadCpm
         self.reads['/random'] = self.doReadRandom
+        self.fileName = harvestFile
 
     # Filesystem methods
     # ==================
     def getattr(self, path, fh=None):
         if path not in self.files:
             raise FuseOSError(ENOENT)
-        
+
+        self.files[path]['st_ctime'] = os.stat(self.fileName).st_ctime
+        self.files[path]['st_mtime'] = os.stat(self.fileName).st_mtime
+        self.files[path]['st_atime'] = os.stat(self.fileName).st_atime
+
         return self.files[path]
 
     def read(self, path, length, offset, fh):
@@ -43,9 +52,46 @@ class GeigerFS(LoggingMixIn, Operations):
         return ['.', '..'] + [x[1:] for x in self.files if x != '/']
 
     def open(self, path, flags):
+        self.timesfhs[path] = open(self.fileName)
         return Operations.open(self, path, flags)
 
     def doReadCpm(self, path, length, offset, fh):
+        ''' returns the count of events detected per minute '''
+        cpm = 0         # calculated counts per minute
+        last_ts = 0.0   # the last time stamp in the time stamps file
+        tfh = self.timesfhs[path]   # get the file handle to the times file for this instance
+        times_offset = 1024     # initial offset from for reading from the end of the file
+        time_diff = 0   # time difference from intervals
+        is_at_end_of_file = False   # have we ended up reading the whole file
+
+        # Read in increments of 1k chunks until we read the whole file,
+        # or we get a minute's worth of events.
+        while ((not is_at_end_of_file) and time_diff < 60):
+            # Check to see if file is larger than current offset for times file:
+            if (os.path.getsize(self.fileName) > times_offset):
+                tfh.seek(-times_offset, os.SEEK_END)
+                tstamps = tfh.readlines()
+                del tstamps[0]  # remove first element since it might be a partial time stamp
+            else:   # otherwise read the whole file
+                tstamps = tfh.readlines()
+                is_at_end_of_file = True
+            if (last_ts == 0.0):    # get the latest time stamp once
+                try: last_ts = float(tstamps[-1])
+                except: pass
+            for ts in reversed(tstamps):    # for each time stamp going backwards
+                try:
+                    if ( not ts ):
+                        break
+                    else:
+                        time_diff = last_ts - float(ts)
+                    if (time_diff < 60.0):
+                        cpm += 1
+                    else:
+                        break   # go until we reach a minute
+                except ValueError:
+                    logging.exception('')
+            times_offset += 1024
+        self.data[path] = str( cpm ) + '\n'
         return self.data[path][offset:offset + length]
 
     def doReadRandom(self, path, length, offset, fh):
